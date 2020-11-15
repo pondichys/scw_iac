8990a39a6530dec962137fde10682b60e25b022e
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2020-11-15 09:38:55 +0000

    Add variables.tf

--- terraform/scw-k8s.tf
+++ terraform/scw-k8s.tf
@@ -1,12 +1,21 @@
 resource "scaleway_k8s_cluster_beta" "scw-k8s-seblab" {
   name = "scw-k8s-seblab"
-  version = "1.19.0"
-  cni = "cilium"
+  version = var.kapsule_k8s_version
+  cni = var.kapsule_cni
 }
 
 resource "scaleway_k8s_pool_beta" "scw-k8s-seblab-workers" {
   cluster_id = scaleway_k8s_cluster_beta.scw-k8s-seblab.id
   name = "scw-k8s-seblab-workers"
-  node_type = "DEV1-M"
-  size = 1
+  node_type = var.kapsule_node_type
+  size = var.kapsule_pool_size
+}
+
+resource "local_file" "kubeconfig" {
+  content = scaleway_k8s_cluster_beta.scw-k8s-seblab.kubeconfig[0].config_file
+  filename = "${path.module}/kubeconfig"
+}
+
+output "cluster_url" {
+  value = scaleway_k8s_cluster_beta.scw-k8s-seblab.apiserver_url
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "fr-par"
+}
+variable "zone" {
+  default = "fr-par-1"
+}
+
+# Kapsule
+variable "kapsule_k8s_version" {
+  default = "1.19.1"
+}
+
+variable "kapsule_cni" {
+    default = "cilium"
+}
+
+variable "kapsule_node_type" {
+  default = "DEV1-M"
+}
+variable "kapsule_pool_size" {
+  default = 1
+}
+variable "kapsule_ingress" {
+  default = "nginx"
+}
