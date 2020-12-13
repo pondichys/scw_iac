35548e1116a46f1adc9e626210211ba82b9cf5e8
pondichys <sebastien.pondichy@gmail.com>
2020-12-13 11:21:57 +0100

    Pin scw provider to 1.17.2 Add container registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,7 @@ terraform {
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
+      version = "~> 1.17.2"
     }
   }
   required_version = ">= 0.13"
---
+++ terraform/scw-container-reg.tf
@@ -0,0 +1,5 @@
+resource "scaleway_registry_namespace" "scw_cr_tf" {
+    name = "seblabcr_priv"
+    description = "SEBLAB private container registry"
+    is_public = false
+}
