b3dc7097346ca3167f3c997e0859ace1ba59dfd8
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2020-12-28 16:38:17 +0000

    Add new object storage for restic backups

--- terraform/scw-blob.tf
+++ terraform/scw-blob.tf
@@ -1,3 +1,7 @@
 resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
   name = "sebpon-nas-backup"
 }
+
+resource "scaleway_object_bucket" "tf-seblab-restic-backup" {
+  name = "seblab-restic-backup"
+}
