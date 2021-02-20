df3d019aeef697c2616470b15da8f19ed43cc2dd
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2021-02-20 07:01:55 +0100

    feat(terraform): update Scaleway provider 1.16 to 2.0.0-rc1
    BREAKING CHANGE: new name for some k8s resources and docker registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
-      version = "~> 1.16.0"
+      version = "2.0.0-rc1"
     }
   }
   required_version = ">= 0.13"
--- terraform/scw-k8s.tf
+++ terraform/scw-k8s.tf
@@ -1,6 +1,6 @@
 # All info available at https://developers.scaleway.com/en/products/k8s/api/#introduction
 
-resource "scaleway_k8s_cluster_beta" "scw-k8s-seblab" {
+resource "scaleway_k8s_cluster" "scw-k8s-seblab" {
   name = "scw-k8s-seblab"
   description = "SEBLAB Kubernetes cluster"
   version = var.kapsule_k8s_version
@@ -8,8 +8,8 @@ resource "scaleway_k8s_cluster_beta" "scw-k8s-seblab" {
   ingress = var.kapsule_ingress
 }
 
-resource "scaleway_k8s_pool_beta" "scw-k8s-seblab-workers" {
-  cluster_id = scaleway_k8s_cluster_beta.scw-k8s-seblab.id
+resource "scaleway_k8s_pool" "scw-k8s-seblab-workers" {
+  cluster_id = scaleway_k8s_cluster.scw-k8s-seblab.id
   name = "scw-k8s-seblab-workers"
   node_type = var.kapsule_node_type
   size = var.kapsule_pool_size
