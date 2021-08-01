b9060f1614997114e99267180752a53a97ea3e47
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2021-08-01 10:49:25 +0200

    delete unused BLOB storage

--- terraform/scw-blob.tf
+++ terraform/scw-blob.tf
@@ -1,11 +1,3 @@
 resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
   name = "sebpon-nas-backup"
 }
-
-resource "scaleway_object_bucket" "tf-seblab-restic-backup" {
-  name = "seblab-restic-backup"
-}
-
-resource "scaleway_object_bucket" "tf-seblab-loki-logs" {
-  name = "seblab-loki-logs"
-}
