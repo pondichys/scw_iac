b9168285c088b2a1f404ad4bc4a6a627a421d8e5
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2021-08-01 10:57:59 +0200

    Delete unused security group

--- terraform/scw-compute.tf
+++ terraform/scw-compute.tf
@@ -1,22 +1,22 @@
 # This file contains all compute resource
 
 # First we create a security group
-resource "scaleway_instance_security_group" "def-sg" {
-  name                   = "seblab-sg"
-  description            = "Security group for seblab.be infra"
-  inbound_default_policy = "drop" # By default we drop incomming trafic that do not match any inbound_rule
+# resource "scaleway_instance_security_group" "def-sg" {
+#   name                   = "seblab-sg"
+#   description            = "Security group for seblab.be infra"
+#   inbound_default_policy = "drop" # By default we drop incomming trafic that do not match any inbound_rule
 
-  inbound_rule { # Accept only SSH trafic from my IP
-    action = "accept"
-    port   = 22
-    ip     = "109.88.57.95"
-  }
+#   inbound_rule { # Accept only SSH trafic from my IP
+#     action = "accept"
+#     port   = 22
+#     ip     = "109.88.57.95"
+#   }
 
-  inbound_rule {
-    action = "accept" # Accept https from anywhere
-    port   = 443
-  }
-}
+#   inbound_rule {
+#     action = "accept" # Accept https from anywhere
+#     port   = 443
+#   }
+# }
 
 # Then we create a public IP
 # resource "scaleway_instance_ip" "server_ip" {}
