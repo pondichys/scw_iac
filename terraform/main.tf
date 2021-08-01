84a125d19d36c75e15dd3f1e95f7f8330f4973fb
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2021-08-01 11:01:48 +0200

    Upgrade terraform version to greater than or equal to 1.0.0 Upgrade Scaleway provider to greater than or equal to 2.1.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,10 @@ terraform {
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
-      version = "2.1.0"
+      version = ">=2.1.0"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.0.0"
 }
 
 provider "scaleway" {
