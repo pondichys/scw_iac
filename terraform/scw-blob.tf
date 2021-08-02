3e209aefc5dc31d868475a63a55b9c3bea4fe633
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2021-08-02 12:34:35 +0200

    add BLOB storage for k8s backups

--- terraform/scw-blob.tf
+++ terraform/scw-blob.tf
@@ -1,3 +1,7 @@
 resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
   name = "sebpon-nas-backup"
 }
+
+resource "scaleway_object_bucket" "tf-seblab-k8s-backup" {
+  name = "seblab-k8s-backup"
+}
