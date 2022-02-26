881a3f89923226e28a51aa5efb8701d95a88f4e8
Sebastien Pondichy <sebastien.pondichy@gmail.com>
2022-02-26 09:23:30 +0100

    add blob storage for linux backups

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "seblab_k8s_cluster_url" {
 output "seblab_k8s_nodes_dns" {
     value = scaleway_k8s_cluster.scw-k8s-seblab.wildcard_dns
 }
+
+output "sebpon_linux_backup" {
+    value = scaleway_object_bucket.tf_sebpon_linux_backup.endpoint
+}
--- terraform/scw-blob.tf
+++ terraform/scw-blob.tf
@@ -5,3 +5,7 @@ resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
 resource "scaleway_object_bucket" "tf-seblab-k8s-backup" {
   name = "seblab-k8s-backup"
 }
+
+resource "scaleway_object_bucket" "tf_sebpon_linux_backup" {
+  name = "sebpon-linux-backup"
+}
