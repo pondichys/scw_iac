fa498779e02561e33886f60756ffb7c6f80316a3
pondichys <sebastien.pondichy@gmail.com>
2022-07-14 13:31:15 +0200

    feat: add subdomains blue and green to seblab.be (#10)
    * feat: add subdomains blue and green to seblab.be
    
    * fix: add formatting and doc

---
+++ terraform/scw-dns.tf
@@ -0,0 +1,9 @@
+resource "scaleway_domain_zone" "blue" {
+  domain    = "seblab.be"
+  subdomain = "blue"
+}
+
+resource "scaleway_domain_zone" "green" {
+  domain    = "seblab.be"
+  subdomain = "green"
+}
