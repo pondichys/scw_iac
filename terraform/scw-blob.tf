9c6c4302eaee43f06a1026c7580cf03062beb862
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2022-09-25 18:37:54 +0200

    Comment all object storages

--- terraform/scw-blob.tf
+++ terraform/scw-blob.tf
@@ -1,18 +1,18 @@
-resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
-  name = "sebpon-nas-backup"
-}
+# resource "scaleway_object_bucket" "tf-sebpon-nas-backup" {
+#   name = "sebpon-nas-backup"
+# }
 
-resource "scaleway_object_bucket" "tf-seblab-k8s-backup" {
-  name = "seblab-k8s-backup"
-  lifecycle_rule {
-    id      = "delete-after-30-days"
-    enabled = true
-    expiration {
-      days = 30
-    }
-  }
-}
+# resource "scaleway_object_bucket" "tf-seblab-k8s-backup" {
+#   name = "seblab-k8s-backup"
+#   lifecycle_rule {
+#     id      = "delete-after-30-days"
+#     enabled = true
+#     expiration {
+#       days = 30
+#     }
+#   }
+# }
 
-resource "scaleway_object_bucket" "tf_sebpon_linux_backup" {
-  name = "sebpon-linux-backup"
-}
+# resource "scaleway_object_bucket" "tf_sebpon_linux_backup" {
+#   name = "sebpon-linux-backup"
+# }
